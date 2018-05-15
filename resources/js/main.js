3be4fe0349e0ffda3d7a4596bb1b5913fa63cbf5
Hakon <32593278+hakonbja@users.noreply.github.com>
2018-05-15 22:43:17 +0200

    Local highscore on two difficulties added

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	restart();
 	//only for testing
-	//$('.title').click(renderAnswer);
+	$('.title').click(renderAnswer);
 });
 
 
@@ -11,16 +11,19 @@ const GREY = 'rgb(200, 200, 200)';
 
 const BLACK = 'rgb(33, 37, 41)';
 
-const RED = 'rgb(255, 81, 93)';
-const REDHEX = '#ff515d';
+const RED = 'rgb(224, 59, 14)'//'rgb(255, 81, 93)';
+const REDHEX = '#E03B0E'//'#ff515d';
 
-/*const GREEN = 'rgb(132, 176, 140)' //#84B08C
-const GREENHEX = '#84B08C';*/
-
-const GREEN = 'rgb(88,148,17)'; //'#589411';
+const GREEN = 'rgb(88, 148, 17)'; //'#589411';
 const GREENHEX = '#589411';
 
-const COLORS = ['rgb(255, 251, 240)', 'rgb(33, 37, 41)', 'rgb(255, 81, 93)', 'rgb(88, 148, 17)', 'rgb(81, 180, 255)', 'rgb(247, 235, 73)'];
+const BLUEHEX = '#2340A1';
+const BLUE = 'rgb(35, 64, 161)'; //'rgb(81, 180, 255)'
+
+const YELLOWHEX = '#E0C80E';
+const YELLOW = 'rgb(224, 200, 14)'; //'rgb(247, 235, 73)'
+
+const COLORS = ['rgb(255, 251, 240)', BLACK, RED, GREEN, BLUE, YELLOW];
 
 
 /* Variables */
@@ -49,30 +52,19 @@ function restart() {
 	flag = true;
 	//reset turn counter and return color to black
 	$('#turn').empty().append('<h5>' + turn + '</h5>').css('color', '#212529');
-	$('.answer i').css('color', GREY);
+	$('.answer i').css('opacity', '0');
+	$('.question-mark').css({'opacity':'1', 'color':BLACK});
 	clearInterval(timer);
 };
 
 function onWin() {
 		renderAnswer();
 		removeGuessHandlers();
-		var minutes = Math.floor((timer) / 60);
-		//var seconds = timer-(60*minutes);
-		var seconds = (timer) % 60;
-		minutes = (minutes < 10) ? '0' + minutes : minutes;
-		seconds = (seconds < 10) ? '0' + seconds : seconds;
-		var tries = 11-turn;
-		if (tries === 1) {
-			$('#win-turns').html('You guessed the right combination in 1 try.');
-		} else {
-			$('#win-turns').html('You guessed the right combination in ' + tries + ' tries.');
-		}
-		$('#win-timer').html('It took you ' + minutes + ':' + seconds + '.');
-		$('#winModal').fadeIn('slow');
+		highScore();
 }
 
+
 function createAnswer() {
-	//var colors = ['rgb(255, 251, 240)', 'rgb(76, 76, 76)', 'rgb(255, 81, 93)', 'rgb(81, 255, 156)', 'rgb(81, 180, 255)', 'rgb(247, 235, 73)'];
 	if (difficulty === 0) {
 		var answer = createAnswerEasy();
 	} else {
@@ -92,7 +84,7 @@ function createAnswerEasy() {
 		answer.push(randomColor);
 		possibleColors.splice(randomNumber, 1);
 	}
-	$('#difficulty').css('color', GREEN);
+	$('#difficulty').html('<h5>Easy</h5>').css('color', GREEN);
 	$('.header-2').css('box-shadow', '0px 1px 2px ' + GREENHEX + ', 0px -1px 2px' + GREENHEX);
 	return answer;
 }
@@ -106,7 +98,7 @@ function createAnswerHard() {
 		var randomColor = possibleColors[randomNumber];
 		answer.push(randomColor);
 	}
-	$('#difficulty').css('color', 'rgb(255, 81, 93)');
+	$('#difficulty').html('<h5>Hard</h5>').css('color', RED);
 	$('.header-2').css('box-shadow', '0px 1px 2px ' + REDHEX + ', 0px -1px 2px' + REDHEX);
 
 	return answer;
@@ -117,6 +109,8 @@ function createAnswerHard() {
 function renderAnswer() {
 	for (i = 0; i < 4; i++) {
 		$('.answer-' + (i+1)).children().css('color', answer[i]);
+		$('.answer i').css('opacity', '1');
+		$('.question-mark').css('opacity', '0');
 	}
 }
 
@@ -146,8 +140,8 @@ function renderGuessRClick() {
 function confirmGuess() {
 	var guess = [];
 
-	for (i = 0; i < 4; i++) {
-		var g = $('.guesses:first .guess-' + (i + 1)).children().css('color');
+	for (i = 1; i < 5; i++) {
+		var g = $('.guesses:first .guess-' + i).children().css('color');
 		guess.push(g);
 	}
 	$('.confirm i').css('color', GREEN);
@@ -158,7 +152,7 @@ function confirmGuess() {
 	var tempAnswer = answer.slice();
 
 	for (guessIndex = 0; guessIndex < guess.length; guessIndex++) {
-		if (tempAnswer[guessIndex]  === guess[guessIndex]) {
+		if (guess[guessIndex] === tempAnswer[guessIndex]) {
 			//add a white pin to indicatorPins
 			indicatorPins.push('white');
 			guess.splice(guessIndex, 1);
@@ -169,7 +163,7 @@ function confirmGuess() {
 	}
 	for (guessIndex = 0; guessIndex < guess.length; guessIndex++) {
 		for (answerIndex = 0; answerIndex < tempAnswer.length; answerIndex++) {
-			if (tempAnswer[answerIndex] === guess[guessIndex]) {
+			if (guess[guessIndex] === tempAnswer[answerIndex]) {
 				indicatorPins.push(BLACK);
 				guess.splice(guessIndex, 1);
 				tempAnswer.splice(answerIndex, 1);
@@ -181,15 +175,16 @@ function confirmGuess() {
 
 	//render indicatorPins
 	i = 0;
-	timeout();
-	function timeout() {
+	renderIndicatorPins();
+
+	function renderIndicatorPins() {
 		setTimeout(function(){
 			$('.indicatorpins:first .pin-' + (i + 1)).animate({color: indicatorPins[i]}, 300);
 			i++;
 			if (i < indicatorPins.length) {
-				timeout();
+				renderIndicatorPins();
 			} else {
-				//remove old handlers
+				//remove old click handlers
 				removeGuessHandlers();
 				//return if won
 				if (hits === 4) {
@@ -313,6 +308,54 @@ function removeGuessHandlers() {
 	for (i = 1; i < 5; i++) {
 		$('.guesses:first .guess-' + i).off();
 	}
-	//$('.guesses:first .guess-1, .guesses:first .guess-2, .guesses:first .guess-3, .guesses:first .guess-4').off();
 	$('.confirm:first .fa-check').off();
+}
+
+
+/* Score functions */
+
+function highScore() {
+	/* Current score */
+	var minutes = Math.floor((timer) / 60);
+	var seconds = (timer) % 60;
+	minutes = (minutes < 10) ? '0' + minutes : minutes;
+	seconds = (seconds < 10) ? '0' + seconds : seconds;
+	var tries = 11-turn;
+	if (tries === 1) {
+		$('#win-turns').html('You guessed the right combination in 1 try.');
+	} else {
+		$('#win-turns').html('You guessed the right combination in ' + tries + ' tries.');
+	}
+	$('#win-timer').html('It took you ' + minutes + ':' + seconds + '.');
+
+	/* High score */
+	var prefix = (difficulty == 0) ? 'green' : 'red';
+	var lsTries = localStorage.getItem(prefix + 'tries');
+	var lsTimer = localStorage.getItem(prefix + 'timer');
+	if (typeof(Storage) !== "undefined") {
+	    // Code for localStorage/sessionStorage.
+
+			if (!lsTries || lsTries > tries || (lsTries == tries && lsTimer > timer)) {
+				console.log("new high score set");
+				localStorage.setItem(prefix + 'tries', tries);
+				localStorage.setItem(prefix + 'timer', timer);
+			}
+
+			var hsTries = localStorage.getItem(prefix + 'tries');
+			var hsMinutes = Math.floor((Number(localStorage.getItem(prefix + 'timer')) / 60));
+			var hsSeconds = localStorage.getItem(prefix + 'timer') % 60;
+			hsMinutes = (hsMinutes < 10) ? '0' + hsMinutes : hsMinutes;
+			hsSeconds = (hsSeconds < 10) ? '0' + hsSeconds : hsSeconds;
+
+			var hsHtml = 'Your high score on the ' + prefix + ' difficulty is ' + hsTries + ' tries in ' + hsMinutes + ':' + hsSeconds + '.';
+			$('#highscore').html(hsHtml);
+	}
+	$('#winModal').fadeIn('slow');
+}
+
+function resetScore() {
+	localStorage.removeItem('tries');
+	localStorage.removeItem('timer');
+	$('#status').html('High score reset.')
+	console.log("High score reset.");
 }
